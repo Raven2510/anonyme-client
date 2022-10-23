74c49dacc5ea73a8a7f3cc7b1e254dcdae31a4de
Raven <raydree13@gmail.com>
2022-10-23 12:12:34 -0600

    added react app

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import app from './App.module.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [name, setName] = useState("");
+
+  function updateName(e){
+    setName(e.target.value);
+  }
+
+  return (
+    <>
+      <h1 className={app.greet}>
+        {
+          name
+        }
+      </h1>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+      }}>
+        <input className={app.input} type="text" value={name} onChange={updateName} placeholder="Enter your name..."></input>
+      </form>
+    </>
+  );
+}
+
+export default App;
